900493977d9a5972e570bd05f110a10b6f153ecd
BenjaminGrayDev <benjamin.gray.dev@gmail.com>
2025-01-28 17:40:31 -0500

    fix users

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -1,7 +1,7 @@
 import type { CollectionConfig } from 'payload'
 
 export const Users: CollectionConfig = {
-  slug: 'users_curriculum',
+  slug: 'users',
   admin: {
     useAsTitle: 'email',
   },
